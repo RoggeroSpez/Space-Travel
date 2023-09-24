714535239ab40138986caf85707d829752c838e9
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-09-24 12:57:35 -0700

    Test

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,22 @@
+import {NavLink} from "react-router-dom";
+import styles from "./NavigationBar.module.css";
+function NavigationBar ()
+{
+  const routes = [
+    {to: "/", text:"Home"},
+    {to: "/spacecrafts", text: "🥔Spacecrafts🥔"},
+    {to: "/planets", text: "🥔Planets🥔"}
+  ];
+  return (
+    <nav className="navigation">
+        {
+      routes.map((route, index) => <NavLink key={index} className={({isActive, isPending}) =>
+      `${styles["navigation__item"]} 
+      ${isActive ? styles["navigation__item--active"] : ""}`} 
+      to={route.to}>{route.text}
+      </NavLink>)
+    }
+    </nav>
+  );
+}
+export default NavigationBar;
